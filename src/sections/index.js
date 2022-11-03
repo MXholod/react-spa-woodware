67e4a1c0c155e10b9ee596420a5d5923627c4ee7
Michael Holod <git_ara@ukr.net>
2022-11-03 18:27:05 +0200

    Created index.js file for convinient export site sections

---
+++ src/sections/index.js
@@ -0,0 +1,7 @@
+export { default as AboutUs } from './about-us/AboutUs';
+export { default as Categories } from './categories/Categories';
+export { default as Footer } from './footer/Footer';
+export { default as Header } from './header/Header';
+export { default as HowItWorks } from './how-it-works/HowItWorks';
+export { default as Main } from './main/Main';
+export { default as Testimony } from './testimony/Testimony';
