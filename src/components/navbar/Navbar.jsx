2b767c321869f00da72898715e21d1f16fd1c895
Michael Holod <git_ara@ukr.net>
2022-11-06 19:09:09 +0200

    Basic template for Navigation bar is prepared

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './navbar.css';
+import { AiOutlineHome } from 'react-icons/ai';
 
 const Navbar = ()=>{
-    return (<div>
-        <h1>Navbar</h1>
-    </div>);
+    return (<nav className="navbar">
+        <ul>
+            <li><AiOutlineHome /><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">How it works</a></li>
+            <li><a href="#">Categories</a></li>
+            <li><a href="#">Testimony</a></li>
+        </ul>
+    </nav>);
 }
 
 export default Navbar;
