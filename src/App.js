c312e9a14b68b8687b8a15c16910b30317d7122b
Michael Holod <git_ara@ukr.net>
2022-11-06 19:17:19 +0200

    HTML Element was removed in App component. Its CSS has been changed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Navbar } from './components';
 function App() {
   return (
     <div className="App">
-      <h1>React SPA wood products</h1>
       <header className="App-header">
         <Header>
           <Navbar />
