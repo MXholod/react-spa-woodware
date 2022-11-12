93b7037d34f084c11f2c9008394e85682cd6ca30
Michael Holod <git_ara@ukr.net>
2022-11-12 18:14:02 +0200

    The site logo has been attached to the header and animated

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './header.css';
+import logo from './../../assets/Logo.png';
 
 const Header = (props)=>{
     return (<div className='header'>
-        <div className='logo'>Woodies</div>
+        <div className='logo'><img src={logo} alt="logo" />
+            Woodies
+        </div>
         { props.children }
     </div>);
 }
