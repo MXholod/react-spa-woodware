b69e106b0e9d73c52b5c6bee6c5b27fc97c49322
Michael Holod <git_ara@ukr.net>
2022-11-21 20:16:22 +0200

    The main section has been finished

--- src/sections/main/Main.jsx
+++ src/sections/main/Main.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import './main.css';
 
 const Main = ()=>{
-    return (<div>
-        <h1>Main</h1>
+    return (<div className='main'>
+        <div className='main__text'>
+            <p>Are you looking for <b>woodden furniture</b> for your place?</p>
+            <div>This is the Right Place</div>
+            <button className='main__button'>Explore Furniture</button>
+        </div>
+        <div className='main__image'></div>
     </div>);
 }
 
