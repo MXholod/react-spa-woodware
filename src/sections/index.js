694d007c933e5d483cc1357387725c348103c734
Michael Holod <git_ara@ukr.net>
2022-12-08 18:46:30 +0200

    The file 'index.js' from 'sections' folder exports the component 'Divider'

--- src/sections/index.js
+++ src/sections/index.js
@@ -5,3 +5,4 @@ export { default as Header } from './header/Header';
 export { default as HowItWorks } from './how-it-works/HowItWorks';
 export { default as Main } from './main/Main';
 export { default as Testimony } from './testimony/Testimony';
+export { default as Divider } from './section-divider/SectionDivider';
