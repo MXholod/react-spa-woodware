3644a3a0a96ed4063a989c74af25c230d2efba16
Michael Holod <git_ara@ukr.net>
2022-12-08 18:08:31 +0200

    The component that divides sections between of each other has been created

---
+++ src/sections/section-divider/SectionDivider.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './section-divider.css';
+import { AiOutlineLine } from 'react-icons/ai';
+import logo from './../../assets/Logo.png';
+
+const SectionDivider = (props)=>{
+    return (<div className='section-divider'>
+        <img src={logo} alt="logo" width="25" height="25" />
+        <span>
+            {props.sectionDescription} 
+            <AiOutlineLine className='section-divider__dash' /> 
+            { props.sectionNameBelow }
+        </span>
+    </div>)
+}
+
+export default SectionDivider;
