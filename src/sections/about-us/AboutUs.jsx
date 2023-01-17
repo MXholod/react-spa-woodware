47fee72c57d07790b0b397d20d5ad1b03f6f8b66
Michael Holod <git_ara@ukr.net>
2023-01-17 23:04:17 +0200

    The section 'About us' has been created

--- src/sections/about-us/AboutUs.jsx
+++ src/sections/about-us/AboutUs.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import './about-us.css';
 
 const AboutUs = ()=>{
-    return (<div>
-        <h1>About us</h1>
-    </div>);
+    return (<section className='about-us'>
+        <div className='about-us__cell'>
+            <div></div>
+            <div>
+                <p><span>WOODIES</span> is the <span>home of modern wooden furniture</span></p>
+                <p>the answer to your need for furniture with shapes, sizes and colors. </p>
+            </div>
+            <div>
+                <p>Customized furniture <span>made just for you</span></p>
+                <p>Get it easy to adjust furniture to the shape and size of your dwelling or business.</p>
+            </div>
+            <div></div>
+        </div>
+    </section>);
 }
 
 export default AboutUs;
