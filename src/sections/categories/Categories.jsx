8dfc130f299ac197c21a47f13dc33049c098502c
Michael Holod <git_ara@ukr.net>
2023-01-19 00:31:22 +0200

    The section 'How it works' has been created and styled

--- src/sections/categories/Categories.jsx
+++ src/sections/categories/Categories.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './categories.css';
+import { getCategories } from './../../services/categoriesService';
 
 const Categories = ()=>{
-    return (<div>
-        <h1>Categories</h1>
-    </div>);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(()=>{
+        getCategories(function(data){
+            setCategories(data);
+        });
+    },[]);
+
+    const handleCategory = (categoryId)=>{
+        console.log("Category id: ",categoryId);
+    }
+
+    return (<section className="categories">
+        <div className="categories__block">
+            <ul className="categories__items">
+            { !!categories.length && categories.map(category => {
+                let align = category.align === 'auto' ? 
+                    '0 auto' : `0 0 0 ${category.align}`;
+                return (<li key={ category.id } className="categories__item">
+                    <div className="categories__title">{ category.title }</div>
+                    <div className="categories__image" 
+                        style={ 
+                            { margin: align }  
+                        }>
+                        <img src={require(`./../../assets/categories/${category.img}`)} 
+                        alt={`Category ${category.title}`} />
+                    </div>
+                    <button onClick={ ()=>handleCategory(category.id) }
+                        className="categories__button">
+                        Shop Now
+                    </button>    
+                </li>);
+            }) }
+            </ul>
+        </div>
+    </section>);
 }
 
 export default Categories;
