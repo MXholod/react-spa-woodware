0a80d1778cab310824a542106ea65dcec8168041
Michael Holod <git_ara@ukr.net>
2023-01-26 02:24:23 +0200

    The function that handles the clicking of A elements has been removed from the NavMenu component

--- src/components/nav-menu/NavMenu.jsx
+++ src/components/nav-menu/NavMenu.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { AiOutlineHome } from 'react-icons/ai';
 import './nav-menu.css';
+import { handleNavMenu } from './../../utils/handle-menu-buttom';
 
 const NavMenu = ()=>{
     return (<>
         <ul className="nav-menu">
-            <li><AiOutlineHome /><a href="#home">Home</a></li>
-            <li><a href="#aboutus">About us</a></li>
-            <li><a href="#howitworks">How it works</a></li>
-            <li><a href="#categories">Categories</a></li>
-            <li><a href="#testimony">Testimony</a></li>
+            <li><AiOutlineHome />
+                <a href="#home" onClick={ handleNavMenu }>Home</a>
+            </li>
+            <li>
+                <a href="#aboutus" onClick={ handleNavMenu }>About us</a>
+            </li>
+            <li>
+                <a href="#howitworks" onClick={ handleNavMenu }>How it works</a>
+            </li>
+            <li>
+                <a href="#categories" onClick={ handleNavMenu }>Categories</a>
+            </li>
+            <li>
+                <a href="#testimony" onClick={ handleNavMenu }>Testimony</a>
+            </li>
             <li>
                 <ul>
                     <li><button>Sign up</button></li>
