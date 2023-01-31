e616f16cf9ec9a186c22013737cd388277136f01
Michael Holod <git_ara@ukr.net>
2023-01-31 16:29:50 +0200

    The component 'SignInForm' has been created

---
+++ src/components/signInForm/SignInForm.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useContext } from 'react';
+import './signInForm.css';
+import { ApplicationContext } from '../../context/appContext';
+
+const SignUpForm = ()=>{
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState(false);
+    const appCtx = useContext(ApplicationContext);
+
+    const handleLogin = (e)=>{
+        if(e.target.value.length >= 0){
+            setLogin(s => e.target.value);
+        }
+        return false;
+    }
+    const handlePassword = (e)=>{
+        if(e.target.value.length >= 0){
+            setPassword(s => e.target.value);
+        }
+        return false;
+    }
+
+    const formHandler = (e)=>{
+        e.preventDefault();
+        const result = [false, false];
+        if(login.length >= 3){
+            result[0] = true;
+        }else{ result[0] = false; }
+        if(password.length >= 6){
+            result[1] = true;
+        }else{ result[1] = false; }
+        let i = 0;
+        for(i; i < result.length; i++){
+            if(!result[i]) break;
+        }
+        if(i === 2){
+            //Pass data to the reducer
+            setIncorrect(false);
+        }else{
+            //Entered data is incorrect
+            setIncorrect(true);
+        }
+    }
+
+    return (<div className="signin-form">
+        <button onClick={ ()=>appCtx.setToggleSignIn(false) } className="signin-form__btn-close">
+            Close
+        </button>
+        <form onSubmit={ formHandler } autoComplete="off">
+            <label>Login
+                <input type="text" onChange={ handleLogin } value={ login } />
+            </label>
+            <label>Password
+                <input type="password" onChange={ handlePassword } value={ password } />
+            </label>
+            <input type="submit" />
+        </form>
+        { incorrect && <div className="signin-form__incorrect-data">
+            Entered data is incorrect!
+        </div> }
+    </div>);
+}
+
+export default SignUpForm;
