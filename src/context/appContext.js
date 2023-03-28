50a6f76e2c6dc863962cfbd166689d86dd145375
Michael Holod <git_ara@ukr.net>
2023-03-28 21:58:06 +0300

    The created “context” contains: 'state' and 'dispatch', which are represented by the “useReducer” hook, three properties with setters that open / close the window for 'signIn', 'signUp' and viewing user information

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,13 +1,18 @@
 import React, { createContext, useState } from 'react';
-
+import { useEnhancedReducer } from './../hooks/useEnhancedReducer';
 
 export const ApplicationContext = createContext();
 
 const AppContext = ({ children })=>{
     const [toggleSignUp, setToggleSignUp] = useState(false); 
     const [toggleSignIn, setToggleSignIn] = useState(false); 
+    const [toggleAuth, setToggleAuth] = useState(false);
+    const { state, dispatch } = useEnhancedReducer();
     return (<ApplicationContext.Provider value={ {
-        toggleSignUp, setToggleSignUp, toggleSignIn, setToggleSignIn
+        toggleSignUp, setToggleSignUp, 
+        toggleSignIn, setToggleSignIn, 
+        toggleAuth, setToggleAuth,
+        state, dispatch,
         } }>
         { children }
     </ApplicationContext.Provider>);
